f0f9c733fe9127098332ae3feadf8698c2623fdf
Victoria Carlos <rayacarlos@gmail.com>
2017-10-08 15:06:03 -0400

    Add twilio api, assets

--- friend.py
+++ friend.py
@@ -1,10 +1,18 @@
+#python modules
 import logging
-from random import randint
 import os
+from random import randint
 
-from flask import Flask, render_template
-from flask_ask import Ask, request, session, question, statement
+#flask modules
+from flask import Flask, json, render_template
+from flask_ask import Ask, request, session, question, statement, context, audio, current_stream
 
+#twilo api to notify friend
+from twilio.rest import Client
+twil_sid = "AC63e4c27ab01477e19d5f28b881f81b0d"
+twil_tok = "888d4650e71395f61b8ac7c1f2e76963"
+
+client = Client(twil_sid, twil_tok)
 
 app = Flask(__name__)
 ask = Ask(app, "/")
@@ -13,19 +21,40 @@ logging.getLogger('flask_ask').setLevel(logging.DEBUG)
 
 @ask.launch
 def launch():
-    speech_text = 'Whats wrong Victoria'
+    speech_text = 'Whats wrong Victoria?'
     return question(speech_text).reprompt(speech_text).simple_card('HelloWorld', speech_text)
 
+#sends sms
+#@ask.intent('TalkIntent')
+#def talk_me():
+   
+ #   cheer = "Let me sing you a song to cheer you up. Excuse my voice, it's been awhile."
+  #  sing = 'https://www.dropbox.com/s/da9e42xjbindwpp/golden_girls.mp3?raw=1'
+   # return audio(cheer).play(sing, offset=93000)
 
-#@ask.intent('HelloWorldIntent')
-#def hello_world():
- #   speech_text = 'Hello little andrea'
-  #  return statement(speech_text).simple_card('HelloWorld', speech_text)
-
-@ask.intent('InspireIntent')
+@ask.intent('SadIntent')
 def inspire_me():
     quote_p = render_template('quote'+ str(randint(0,20)))
-    return statement(quote_p)
+    did_help ='..Did that help?'
+    return question(quote_p + did_help)
+
+@ask.intent('SuccessIntent')
+def yes():
+    hey = 'I am glad I could help. I am always here for you.'
+    return statement(hey) #stop the session
+
+@ask.intent('FailedIntent')
+def no():
+    sorry = "I\'m sorry to hear that. I can only do so much, I\'m only a robot after all. Should I contact someone?"
+    return question(sorry)
+
+@ask.intent('TheEnding')
+def human_contact(contact):
+    if (contact == 'mom'):
+        client.api.account.messages.create(to= "+13109930763",from_="+13213604467 ",body="You need to talk to Victoria")
+    elif (contact == 'my best friend'):
+        client.api.account.messages.create(to= "+13868640599",from_="+13213604467 ",body="You need to talk to Victoria")
+    return statement("Got it.")
 
 
 @ask.intent('AMAZON.HelpIntent')
